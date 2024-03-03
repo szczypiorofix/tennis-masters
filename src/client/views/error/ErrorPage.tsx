1641ef2655847850988bb1be94a395603529f908
szczypiorofix <szczypiorofix@o2.pl>
2024-03-03 21:02:15 +0100

    Error page update

--- src/client/views/error/ErrorPage.tsx
+++ src/client/views/error/ErrorPage.tsx
@@ -1,18 +1,36 @@
-import { useRouteError } from "react-router-dom";
+import React from "react";
+import {isRouteErrorResponse, useRouteError} from "react-router-dom";
 
-function ErrorPage() {
-    const error: any = useRouteError();
-    console.error(error);
+import {ErrorResponse} from "@remix-run/router/utils";
 
-    return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-        </div>
-    );
+const ErrorPage: React.FC = () => {
+    const error: ErrorResponse | Error = useRouteError() as ErrorResponse | Error;
+    if (isRouteErrorResponse(error)) {
+        return (
+            <div id="error-page">
+                <h1>Oops!</h1>
+                <h3>{error.status}</h3>
+                <p>{error.statusText}</p>
+                {error.data?.message && (
+                    <p>
+                        <i>{error.data.message}</i>
+                    </p>
+                )}
+            </div>
+        );
+    }
+    else if (error instanceof Error) {
+        return (
+            <div id="error-page">
+                <h1>Oops! Unexpected Error</h1>
+                <p>Something went wrong.</p>
+                <p>
+                    <i>{error.message}</i>
+                </p>
+            </div>
+        );
+    }
+    return <></>;
 }
 
 export default ErrorPage;
