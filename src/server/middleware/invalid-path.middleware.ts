a0a4679af554ddebf0607c23daaa6bd3e1b9877e
szczypiorofix <szczypiorofix@o2.pl>
2024-03-07 00:39:08 +0100

    Add server response as ServerResponse type

--- src/server/middleware/invalid-path.middleware.ts
+++ src/server/middleware/invalid-path.middleware.ts
@@ -1,7 +1,13 @@
-import { NextFunction, Request, Response } from 'express';
+import {NextFunction, Request, Response} from 'express';
+import {ServerResponse} from "../../shared";
 
 const invalidPathHandler =  (req: Request, res: Response, next: NextFunction) => {
-    res.status(404).json({ message: `Invalid path: ${req.path} ` });
+    const serverResponse: ServerResponse = {
+      error: true,
+      message: `Invalid path: ${req.path}`,
+      code: 404
+    };
+    res.status(serverResponse.code).json(serverResponse);
 }
 
 export { invalidPathHandler };
--- src/server/middleware/required-content-type.middleware.ts
+++ src/server/middleware/required-content-type.middleware.ts
@@ -1,8 +1,14 @@
-import { NextFunction, Request, Response } from 'express';
+import {NextFunction, Request, Response} from 'express';
+import {ServerResponse} from "../../shared";
 
 const requiredContentTypeHandler = (req: Request, res: Response, next: NextFunction) => {
     if (req.headers['content-type'] !== 'application/json') {
-        res.status(400).json({ message: 'Content-Type must be application/json' });
+        const serverResponse: ServerResponse = {
+            error: true,
+            message: 'Content-Type must be application/json',
+            code: 400
+        };
+        res.status(serverResponse.code).json(serverResponse);
     } else {
         next();
     }
